01dda63c98a7fb02b8ce8994e2288006e4313eb3
Ankit Kumar <hello@ankdev.me>
2020-03-26 03:47:27 +0530

    use path to join path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-
+const path = require('path');
 const port=9000;
 const app=express();
 const cokkiesparser=require('cookie-parser');
@@ -24,7 +24,7 @@ app.use(express.static('./dist'));
 app.use('/uploads',express.static(__dirname +'/uploads'));
 app.use('/uploads/products/image',express.static(__dirname+'/uploads/products/image'));
 
-app.use(fallback(__dirname + '/dist/index.html'))
+app.use(fallback(path.join(__dirname, '/dist/index.html')))
 
 
 
