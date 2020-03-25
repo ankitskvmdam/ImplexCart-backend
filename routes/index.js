69ccb8d69675df543d30d31918e699ca192143a1
Ankit Kumar <hello@ankdev.me>
2020-03-26 02:09:58 +0530

    server react index folder

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express=require('express');
+const path = require('path');
 
 // using express route
 const router=express.Router();
 
 
-const homeController=require('../controllers/homecontroller');
+// const homeController=require('../controllers/homecontroller');
 
 
-router.get('/',homeController.home);
+// router.get('/',homeController.home);
+
+// https://codeforgeek.com/render-html-file-expressjs/
+router.get('/', function(req, res){
+    const distPath = (path.join(__dirname, '../dist/index.html'))
+    res.sendFile(distPath)
+})
 router.use('/user',require('./user'));
 router.use('/api',require('./api'));
 
