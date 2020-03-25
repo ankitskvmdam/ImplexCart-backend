528792b44d6f1d2c04c27736211ce9e8f946b9c0
Ankit Kumar <hello@ankdev.me>
2020-03-26 01:16:49 +0530

    rollback

--- index.js
+++ index.js
@@ -12,17 +12,7 @@ const MongoStore=require('connect-mongo')(session);
 const passportlocal=require('./config/passport');
 const cors = require('cors')
 
-const whitelist = ['http://18.212.163.39:9000', 'http://localhost:8080', 'http://saleimplexcart.online:9000', 'https://saleimplexcart.online:9000' ]
-
-const corsOptions = {
-	origin: function(origin, callback){
-		if(whitelist.indexOf(origin) != -1) callback(null, true)
-		else callback(new Error('Not Allowed by CORS'))
-	},
-	credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded());
 app.use(cokkiesparser());
 
